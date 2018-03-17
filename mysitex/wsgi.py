c0039cdec4700fec352cdb17133dfef623a1bcd7
Jay Wong <jiae@hotmail.com>
2018-03-17 21:33:57 +0800

    First commit

---
+++ mysitex/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mysitex project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/Users/floshin33/DjangoProject/mysitex')
+sys.path.append('/Users/floshin33/DjangoProject/mysitex/mysitex')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysitex.settings")
+
+application = get_wsgi_application()
